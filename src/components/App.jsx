0cb362494b4d736e967618ba18365a54ee492658
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-02-26 21:54:34 +0530

    update image urls in contacts.js and add a hard-coded card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function App() {
-  return ();
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <div className="card">
+        <div className="top">
+          <h2>Elon Musk</h2>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/330px-Elon_Musk_Royal_Society_%28crop2%29.jpg"
+            alt="avatar_img"
+          />
+        </div>
+        <div className="bottom">
+          <p>+123 456 789</p>
+          <p>elon@spacex.com</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
