1ecd45f631847333956f2ca580dac22f33b587be
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-02-26 22:25:12 +0530

    initial version

--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//4. Import the contacts.js file to create card components.
