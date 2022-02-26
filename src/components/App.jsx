597eedb726f3c1b4f39cd5a227c3101d9e4e40aa
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-02-26 22:05:52 +0530

    remove card specific code from App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,20 +4,6 @@ function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Elon Musk</h2>
-          <img
-            className="circle-img"
-            src="https://i.imgur.com/CNhZzYY.jpeg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>elon@spacex.com</p>
-        </div>
-      </div>
     </div>
   );
 }
