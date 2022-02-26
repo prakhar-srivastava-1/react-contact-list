2c38c2576ae2e3cab6da0c3bf4c5ba9b26278e15
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-02-26 22:07:39 +0530

    add Card component to App - test successful

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import Card from "./Card.jsx";
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Card />
     </div>
   );
 }
